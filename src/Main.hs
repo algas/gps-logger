b4d0492b2467032cd1c86e87ce60c95c57740b01
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2018-11-28 09:28:08 +0900

    Update logics

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,23 @@
 module Main where
 
-import Data.ByteString.Lazy (ByteString)
-import qualified Data.ByteString.Lazy as BL
+import           Data.ByteString.Lazy       (ByteString)
+import qualified Data.ByteString.Lazy       as BL
 import qualified Data.ByteString.Lazy.Char8 as BLC
-import Data.List (mapAccumR)
+import           Data.Geodetic.LL           (LL (..))
+import           Data.List                  (mapAccumR)
+import           GHC.Int                    (Int64)
 
-type Lat = Int
 
-parseLat :: ByteString -> Lat
-parseLat = parse
-
-parse :: ByteString -> Int
-parse bs = sum [f * x | (f, x) <- zip fs lats]
+parse :: Int64 -> ByteString -> Double
+parse index bs = realToFrac res / (100000 :: Double)
     where
-        lats = map fromIntegral $ BL.unpack $ BL.take 3 $ BL.drop 76 bs :: [Int]
+        lats = map fromIntegral $ BL.unpack $ BL.take 3 $ BL.drop index bs :: [Int]
         fs = take (length lats) (iterate (256*) 1)
+        res = sum [f * x | (f, x) <- zip fs lats]
 
 main :: IO ()
 main = do
     f <- BL.readFile "001"
     BLC.putStrLn $ BL.take 9 $ BL.drop 12 f
-    print $ parseLat f
+    let ll = LL {_lat = parse 76 f, _lon = parse 80 f}
+    print ll
